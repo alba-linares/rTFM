6191a6bc8971f4ce8c39fb6163fee755e222a09d
alba-linares <albalinares20@gmail.com>
2024-08-23 12:46:51 +0200

    he creado graficas de usos del suelo por humedal para ambos años y he actualizado el campo de area del MUCVA en la tabla de atributos y en el Excel

--- R/script_analisis_datos.R
+++ R/script_analisis_datos.R
@@ -31,8 +31,8 @@ frecuencia_clase_humedal <- datos %>%
 View(frecuencia_clase_humedal)
 
 
-
-# Gráfico de barras apiladas para mostrar la proporción de cada uso del suelo por humedal
+################################################################################
+# Gráfico de barras apiladas para mostrar la proporción de cada uso del suelo por humedal RELATIVO
 ggplot(datos, aes(x = MUCVA__USO, fill = NOMBRE_HUM)) +
   geom_bar(position = "fill") +
   labs(title = "Proporción de usos del suelo en 1984 por humedal", 
@@ -51,13 +51,36 @@ ggplot(datos, aes(x = SIOSE__USO, fill = NOMBRE_HUM)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=0))
 
+# ABSOLUTO #####################################################################
+ggplot(datos, aes(x = MUCVA__USO, fill = NOMBRE_HUM)) +
+  geom_bar(position = "stack") +  # Cambia "fill" por "stack" para contar valores absolutos
+  labs(title = "Número de hectáreas por uso del suelo en 1984 por humedal", 
+       x = "Uso del suelo", 
+       y = "Hectáreas") +
+  scale_fill_viridis_d(option = "H") + #B o H están bien, se distinguen los humedales
+  ylim(0, 800) +  # Establece el límite del eje y entre 0 y 800
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust = 0))
+
+ggplot(datos, aes(x = SIOSE__USO, fill = NOMBRE_HUM)) +
+  geom_bar(position = "stack") +  # Cambia "fill" por "stack" para contar valores absolutos
+  labs(title = "Número de hectáreas por uso del suelo en la actualidad por humedal", 
+       x = "Uso del suelo", 
+       y = "Hectáreas") +
+  scale_fill_viridis_d(option = "H") + #B o H están bien, se distinguen los humedales
+  ylim(0, 800) +  # Establece el límite del eje y entre 0 y 800
+    theme_minimal() +
+  theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=0))
+
+################################################################################
+
 library(dplyr)
 df_area_usos84 <-read_excel("C:/Users/VE-UGR-0208/Desktop/TFM/rTFM/Resultados_puntos_analisis.xlsx", sheet = 2)
 df_area_usos20 <-read_excel("C:/Users/VE-UGR-0208/Desktop/TFM/rTFM/Resultados_puntos_analisis.xlsx", sheet = 3)
 
 result_area84 <- df_area_usos84 %>%
   group_by(df_area_usos84$`_USOS_DEFI`) %>%
-  summarise(Total_Area = sum(Shape_Area))
+  summarise(Total_Area = sum(AREA_POLIG))
 
 result_area20 <- df_area_usos20 %>%
   group_by(`_USOS_DEFI`) %>%
